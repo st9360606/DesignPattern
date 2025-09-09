fac2c99271c2858f73f47f99a7a967d6c01d66ce
st9360606 <st9360606@gmail.com>
2025-09-09 10:19:09 +0800

    add 模板模式

---
+++ src/main/java/com/example/pattern/template/PeanutSoyaMilk.java
@@ -0,0 +1,11 @@
+package com.example.pattern.template;
+
+public class PeanutSoyaMilk extends SoyaMilk {
+
+	@Override
+	void addCondiments() {
+		// TODO Auto-generated method stub
+		System.out.println(" 加入花生進去... ");
+	}
+
+}
---
+++ src/main/java/com/example/pattern/template/RedBeanSoyaMilk.java
@@ -0,0 +1,11 @@
+package com.example.pattern.template;
+
+public class RedBeanSoyaMilk extends SoyaMilk {
+
+	@Override
+	void addCondiments() {
+		// TODO Auto-generated method stub
+		System.out.println(" 加入紅豆進入... ");
+	}
+
+}
